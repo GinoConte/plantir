e5df18c1f6bb20d909ab2e0eced9d314362e6edf
GinoConte <ginoconte95@gmail.com>
2017-09-21 10:11:54 +1000

    added new models, created /garden/ api routing

---
+++ model/gardens.js
@@ -0,0 +1,14 @@
+'use strict';
+//import dependency
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//garden schema (list of tiles, creation date, id(token), location text(for weather api)
+var GardenSchema = new Schema({
+	location : String,
+	tiles : [{type: Schema.Types.ObjectId, ref: 'Tile'}]
+});
+
+//export our modules to use in server.js
+module.exports = mongoose.model('Garden', GardenSchema);
+
---
+++ model/tiletypes.js
@@ -0,0 +1,25 @@
+'use strict';
+//import dependency
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+//tiletype schema (name, altname(e.g latin/scientific), isPlant, list of requirements)
+var TileTypeSchema = new Schema({
+	name: String,
+	altname: String,
+	isplant: Boolean, 
+	info: String,
+	tilecolour: String,
+	requirements: {
+		watering: String,
+		blossom: String,
+		ph: String,
+		sunlight: String,
+		moisture: String,
+		soiltype: String
+	}
+
+});
+
+//export our modules to use in server.js
+module.exports = mongoose.model('TileType', TileTypeSchema);
