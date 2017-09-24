01ff610c97155bcffbe9603ecbadbe4a1e9c92d0
GinoConte <ginoconte95@gmail.com>
2017-09-24 15:11:13 +1000

    added create button functionality

--- src/WelcomeHeader.js
+++ src/WelcomeHeader.js
@@ -5,22 +5,23 @@ class WelcomeHeader extends Component {
   constructor(props) {
     super(props);
     this.state = { gardenid: '' };
-    this.handleAuthorChange = this.handleAuthorChange.bind(this);
-    this.handleTextChange = this.handleTextChange.bind(this);
     this.handleTokenChange = this.handleTokenChange.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleAuthorChange(e) {
-    this.setState({ author: e.target.value });
-  }
-  handleTextChange(e) {
-    this.setState({ text: e.target.value });
   }
   handleTokenChange(e) {
     this.setState({ gardenid: e.target.value });
   }
+  handleCreate(e) {
+    e.preventDefault();
+    this.setState({ gardenid: '' });
+    this.props.onCreateClicked();
+  }
   handleSubmit(e) {
     e.preventDefault();
+
+
+
     // let author = this.state.author.trim();
     // let text = this.state.text.trim();
     let token = this.state.gardenid.trim();
@@ -32,14 +33,15 @@ class WelcomeHeader extends Component {
     // this.setState({ author: '', text: '' });
     this.props.onTokenSubmit(token)
     this.setState({ gardenid: '' });
+
   }
   render() {
     return (
       <form style={ style.commentForm } onSubmit={ this.handleSubmit }>
-        <input
-          type='submit'
+        <button
           style={ style.commentFormPost }
-          value='Create' />
+          value='Create' 
+          onClick={this.handleCreate} >Create</button>
         <input
           type='text'
           placeholder='Enter a garden token!'
@@ -49,7 +51,7 @@ class WelcomeHeader extends Component {
         <input
           type='submit'
           style={ style.commentFormPost }
-          value='Post' />
+          value='Submit' />
       </form>
     )
   }
