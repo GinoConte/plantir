5eca5af28db3ace542b2b2996cce2878b8c250cf
GinoConte <ginoconte95@gmail.com>
2017-10-20 20:01:57 +1100

    added initial version of timeline, hover over images to see it change according to bloom time

--- src/Timeline.js
+++ src/Timeline.js
@@ -1,164 +1,144 @@
 import style from './style';
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+var AmCharts = require("@amcharts/amcharts3-react");
+//import AmCharts from "@amcharts/amcharts3-react";
 
-import _ from "underscore";
-//import Select from "react-select";
-import colorbrewer from "colorbrewer";
+function generateData(name, bloomString) {
 
-// Pond
+  var dataProvider = [{
+          "season": "Summer",
+          "value": 0,
+          }, {
+          "season": "Autumn",
+          "value": 1
+          }, {
+          "season": "Winter",
+          "value": 0
+          }, {
+          "season": "Spring",
+          "value": 0
+  }];
 
-// Imports from the charts library
-import { Charts, Legend, AreaChart, ChartContainer, ChartRow, YAxis, LineChart, BarChart, Resizable, styler } from "react-timeseries-charts";
-import { TimeSeries, TimeRange, Index } from "pondjs";
-
-// import continents_docs from "./continents_docs.md";
-// import continents_thumbnail from "./continents_thumbnail.png";
-
+  return dataProvider;
+}
 
 class Timeline extends Component {
   constructor(props) {
     super(props);
-    this.state = { gardenid: '' };
-    this.handleTokenChange = this.handleTokenChange.bind(this);
-    this.handleCreate = this.handleCreate.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleTokenChange(e) {
-    this.setState({ gardenid: e.target.value });
-  }
-  handleCreate(e) {
-    e.preventDefault();
-    this.setState({ gardenid: '' });
-    this.props.onCreateClicked();
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    let token = this.state.gardenid.trim();
-    if (!token) {
-      return;
-    }
-    this.props.onTokenSubmit(token)
-    this.setState({ gardenid: '' });
-  }
-  getInitialState() {
-    return {
-        highlight: null,
-        selection: null,
-        scheme: "Paired"
+
+    this.state = {
+      dataProvider: this.updateData(),
+      timer: null
     };
+    this.updateData = this.updateData.bind(this);
   }
-  handleSchemeChange({ value }) {
-    this.setState({ scheme: value });
+  updateData() {
+    let name = this.props.hoverName;
+    let bloom = this.props.hoverBloom;
+
+    let summerval = 0;
+    let springval = 0;
+    let winterval = 0;
+    let autumnval = 0;
+
+    if (bloom.includes("summer")) {
+      summerval = 1;
+    }
+    if (bloom.includes("spring")) {
+      springval = 1;
+    }
+    if (bloom.includes("winter")) {
+      winterval = 1;
+    }
+    if (bloom.includes("autumn") || (bloom.includes("fall"))) {
+      autumnval = 1;
+    }
+
+
+      var data = [{
+          "season": "Summer",
+          "value": summerval
+          }, {
+          "season": "Autumn",
+          "value": autumnval
+          }, {
+          "season": "Winter",
+          "value": winterval
+          }, {
+          "season": "Spring",
+          "value": springval
+       }];
+       this.setState({dataProvider: data});
   }
+
+  componentDidMount() {
+    this.setState({
+      // Update the chart dataProvider every 3 seconds
+      timer: setInterval(() => {this.updateData()}, 1000)
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.timer);
+  }
+
   render() {
-
-  const data = [
-      ["2017-01-24 00:00", 0.01],
-      ["2017-01-24 01:00", 0.13],
-      ["2017-01-24 02:00", 0.07],
-      ["2017-01-24 03:00", 0.04],
-      ["2017-01-24 04:00", 0.33],
-      ["2017-01-24 05:00", 0.2],
-      ["2017-01-24 06:00", 0.08],
-      ["2017-01-24 07:00", 0.54],
-      ["2017-01-24 08:00", 0.95],
-      ["2017-01-24 09:00", 1.12],
-      ["2017-01-24 10:00", 0.66],
-      ["2017-01-24 11:00", 0.06],
-      ["2017-01-24 12:00", 0.3],
-      ["2017-01-24 13:00", 0.05],
-      ["2017-01-24 14:00", 0.5],
-      ["2017-01-24 15:00", 0.24],
-      ["2017-01-24 16:00", 0.02],
-      ["2017-01-24 17:00", 0.98],
-      ["2017-01-24 18:00", 0.46],
-      ["2017-01-24 19:00", 0.8],
-      ["2017-01-24 20:00", 0.39],
-      ["2017-01-24 21:00", 0.4],
-      ["2017-01-24 22:00", 0.39],
-      ["2017-01-24 23:00", 0.28]
-  ];
-
-  const rawData = data;
-  const numPoints = rawData.length;
-  const columnNames = rawData.map(d => d.key);
-
-  const name = "series";
-  const columns = ["time", ...columnNames];
-  const points = [];
-
-  for (let i = 0; i < numPoints; i++) {
-      const t = rawData[0].values[i][0];
-      const point = [t];
-      _.each(rawData, d => {
-          point.push(d.values[i][1]);
-      });
-      points.push(point);
-  }
-
-  const series = new TimeSeries({ name, columns, points });
-
-
-  const cols = { up: columnNames, down: [] };
-  const min = 0;
-  const max = 130;
-  const axisType = "linear";
-  const interpolationType = "curveBasis";
-  const options = Object.keys(colorbrewer).map(c => ({ value: c, label: c }));
-  const style = styler(columnNames, this.state.scheme);
-  const legendCategories = columnNames.map(d => ({ key: d, label: d }));
-
-
+    const config = {
+      "type": "serial",
+      "theme": "patterns",
+      "marginRight": 40,
+      "marginLeft": 40,
+      "autoMarginOffset": 20,
+      "mouseWheelZoomEnabled": true,
+      "valueAxes": [{
+        "id": "v1",
+        "axisAlpha": 0,
+        "position": "left",
+        "ignoreAxisWidth": true
+      }],
+      "balloon": {
+        "borderThickness": 1,
+        "shadowAlpha": 0
+      },
+      "graphs": [{
+        "id":"g1",
+        "balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
+        "bullet": "round",
+        "bulletSize": 8,
+        "lineColor": "#d1655d",
+        "lineThickness": 2,
+        "negativeLineColor": "#637bb6",
+        "type": "smoothedLine",
+        "valueField": "value"
+      }],
+      "chartCursor": {
+        "pan": true,
+        "valueLineEnabled": true,
+        "valueLineBalloonEnabled": true,
+        "cursorAlpha":1,
+        "cursorColor":"#258cbb",
+        "limitToGraph":"g1",
+        "valueLineAlpha":0.2,
+        "valueZoomable": true
+      },
+      "categoryField": "season",
+      "categoryAxis": {
+        "dashLength": 1,
+        "minorGridEnabled": true
+      },
+      "dataProvider": this.state.dataProvider,
+    };
 
     return (
-                  <div>
-                <div className="row">
-                    <div className="col-md-3">
-                    </div>
-                    <div className="col-md-9">
-                        <Legend categories={legendCategories} style={style} type="dot" />
-                    </div>
-                </div>
-                <hr />
-                <div className="row">
-                    <div className="col-md-12">
-                        <Resizable>
-                            <ChartContainer
-                                timeRange={series.range()}
-                                onBackgroundClick={() => this.setState({ selection: null })}
-                            >
-                                <ChartRow height="350">
-                                    <YAxis
-                                        id="value"
-                                        min={min}
-                                        max={max}
-                                        width="60"
-                                        type={axisType}
-                                    />
-                                    <Charts>
-                                        <AreaChart
-                                            axis="value"
-                                            style={style}
-                                            series={series}
-                                            columns={cols}
-                                            fillOpacity={0.4}
-                                            interpolation={interpolationType}
-                                            highlight={this.state.highlight}
-                                            onHighlightChange={highlight =>
-                                                this.setState({ highlight })}
-                                            selection={this.state.selection}
-                                            onSelectionChange={selection =>
-                                                this.setState({ selection })}
-                                        />
-                                    </Charts>
-                                </ChartRow>
-                            </ChartContainer>
-                        </Resizable>
-                    </div>
-                </div>
-</div>
-    )
+      <div className="Timeline">
+        <p>Bloom time for: {this.props.hoverName}</p>
+        <AmCharts.React style={{ width: "100%", height: "200px" }} options={config} />
+      </div>
+    );
+
   }
 }
+
 
 export default Timeline;
